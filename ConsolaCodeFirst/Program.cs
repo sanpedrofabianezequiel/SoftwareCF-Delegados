10a4485622f2e315773a8f1855ae8cadef10ca80
sanpedrofabianezequiel@gmail.com <sanpedrofabianezequiel@gmail.com>
2020-10-20 14:36:33 -0300

    Insertamos el EntityFrameWork al proyecto

--- ConsolaCodeFirst/Program.cs
+++ ConsolaCodeFirst/Program.cs
@@ -10,7 +10,19 @@ namespace ConsolaCodeFirst
     {
         static void Main(string[] args)
         {
+            /*
+             Modelo.Contexto db= new Modelo.Contexto();
+             Modelo.Autor objAutor= new Modelo.Autor();
 
+                objAutor.Apellido= "San Pedro";
+                objAutor.Nombre = "Ezequiel";
+                    db.Autores.Add(objAutor);
+                    db.SaveChanges();
+                        Console.WriteLine("Autor ingresado en la Base de Datos con Exito");
+             
+                            
+             Console.ReadKey();
+             */
             
 
         }
--- Modelo/Contexto.cs
+++ Modelo/Contexto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Modelo
 {
-    class Contexto
+    class Contexto:DbContext
     {
     }
 }
